20db1be4c5acc61bdb16d0e8ae7fe7cf263123e8
MMangus <mjmangus16@yahoo.com>
2019-06-17 14:55:59 -0400

    project setup

---
+++ ToDo/src/Components/TodoList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Todo from "./Todo";
+
+const TodoList = () => {
+  return (
+    <div>
+      HELLO
+      <Todo />
+    </div>
+  );
+};
+
+export default TodoList;
